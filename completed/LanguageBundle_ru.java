ae51926b1eed0a8b2b2216d0f3a2887795b3a988
Serega11112222 <Sergey243218548@gmail.com>
2017-06-04 22:45:26 +0800

    Update LanguageBundle_ru.java

--- completed/LanguageBundle_ru.java
+++ completed/LanguageBundle_ru.java
@@ -24,7 +24,7 @@ import com.nowlive.bot.util.Const;
 import java.util.ListResourceBundle;
 
 /**
- * @author <put your name here>
+ * @author Серёга
  * @version Russian
  */
 public class LanguageBundle_ru extends ListResourceBundle {
